5be2206035599272b4c315734d497f191101dc5e
renzaspiras <aspirasrenz@gmail.com>
2024-04-07 06:05:05 +0800

    bug fixer

--- gpt.py
+++ gpt.py
@@ -5,9 +5,8 @@ command = "nohup ~/apps/ollama/ollama serve >/dev/null 2>&1 &"
 subprocess.Popen(command, shell=True)
 
 def prompt(what):
-  return f"You will answer the prompt as simple as possible. Do not overcomplicate your output. [PROMPT:{what}]";
+    return f"You will answer the prompt as simple as possible. Do not overcomplicate your output. [PROMPT:{what}]"
 
-#os.system();
 ask = input("[You]:> ")
-
-os.system(f"~/apps/ollama/ollama run tinyllama {property(ask)}")
+prompt_message = prompt(ask)
+os.system(f'~/apps/ollama/ollama run tinyllama "{prompt_message}"')
