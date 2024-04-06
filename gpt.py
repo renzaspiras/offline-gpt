aff80ee24e401bf2c5004d874e7004e56a9c0023
renzaspiras <aspirasrenz@gmail.com>
2024-04-07 06:13:53 +0800

    hardenned....

--- gpt.py
+++ gpt.py
@@ -1,12 +1,22 @@
 import subprocess
 import os
 
-command = "nohup ~/apps/ollama/ollama serve >/dev/null 2>&1 &"
-subprocess.Popen(command, shell=True)
+def start_ollama():
+    command = "nohup ~/apps/ollama/ollama serve >/dev/null 2>&1 &"
+    subprocess.Popen(command, shell=True)
 
 def prompt(what):
     return f"You will answer the prompt as simple as possible. Do not overcomplicate your output. [PROMPT:{what}]"
 
-ask = input("[You]:> ")
-prompt_message = prompt(ask)
-os.system(f'~/apps/ollama/ollama run tinyllama "{prompt_message}"')
+start_ollama()
+
+while True:
+    ask = input("[You]:> ")
+    if ask.lower() == "exit":
+        break
+    else:
+        prompt_message = prompt(ask)
+        os.system(f'~/apps/ollama/ollama run tinyllama "{prompt_message}"')
+
+# Close the app
+os.system("pkill -f ollama")
